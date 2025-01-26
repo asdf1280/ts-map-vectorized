71c009845fb780cc90c8705c5ac3606320f18b2f
D <10652364+asdf1280@users.noreply.github.com>
2025-01-27 00:32:51 +0900

    Restore colors

--- TsMap.Canvas/SimpleMapPalette.cs
+++ TsMap.Canvas/SimpleMapPalette.cs
@@ -1,19 +1,16 @@
 ﻿using System.Drawing;
 
-namespace TsMap.Canvas
-{
-    public class SimpleMapPalette : MapPalette
-    {
-        public SimpleMapPalette()
-        {
-            Background = new SolidBrush(Color.FromArgb(0, 0, 0, 0));
-            Road = new SolidBrush(Color.FromArgb(180, 255, 255, 255));
-            PrefabRoad = new SolidBrush(Color.FromArgb(180, 255, 255, 255));
-            PrefabLight = new SolidBrush(Color.FromArgb(180, 255, 255, 255));
-            PrefabDark = new SolidBrush(Color.FromArgb(180, 255, 255, 255));
-            PrefabGreen = new SolidBrush(Color.FromArgb(180, 255, 255, 255)); // TODO: Check if green has a specific z-index
+namespace TsMap.Canvas {
+    public class SimpleMapPalette : MapPalette {
+        public SimpleMapPalette() {
+            Background = new SolidBrush(Color.FromArgb(72, 78, 102));
+            Road = Brushes.White;
+            PrefabRoad = Brushes.White;
+            PrefabLight = new SolidBrush(Color.FromArgb(236, 203, 153));
+            PrefabDark = new SolidBrush(Color.FromArgb(225, 163, 56));
+            PrefabGreen = new SolidBrush(Color.FromArgb(170, 203, 150)); // TODO: Check if green has a specific z-index
 
-            CityName = new SolidBrush(Color.FromArgb(255, 224, 112));
+            CityName = Brushes.LightCoral;
 
             FerryLines = new SolidBrush(Color.FromArgb(80, 255, 255, 255));
 
